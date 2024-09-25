b3faf66a7ba6ac8ef60e4c4ba0296849ba370edf
luoshuiL <3546253608@qq.com>
2024-09-25 19:38:58 +0800

    Add files via upload

--- level2.2.c
+++ level2.2.c
@@ -12,22 +12,23 @@ int main() {
 	int storeH[6] = { 0 };
 	int k = 0;//连续退回操作的数量
 	do {
-		k = 0;//记录回退步数
+		
 		scanf_s("%c", &goods,1);//货物名称     //第一步：装入货物
 		if (goods == 57) {                //退步操作需要
-			if (j == 0||k>3) {
+			if (j == 0||k==3) {
 				printf("无法退回\n");
-				scanf_s("%*c");//清缓存区（网上学的）
-				scanf_s("%*c");//清缓存区（网上学的）   但写两个是我试出来的，不知道为什么
+				scanf_s("%*[^\n]");//清缓存区（网上学的）
+				scanf_s("%*c");//清缓存区（网上学的）  
 				continue;
 			}
-			else if (k < 4) {
+			else if (k < 3) {
+				there:
 				k++;
 				pass = storeH[j];
 				passN[pass] = 0;
 				j--;
-				printf("已成功退回\n");
-				scanf_s("%*c");//清缓存区（网上学的）
+				printf("已成功退回通道%d商品,请继续装填\n",pass);
+				scanf_s("%*[^\n]");//清缓存区（网上学的）
 				scanf_s("%*c");//清缓存区（网上学的）
 				continue;
 			}
@@ -45,21 +46,22 @@ int main() {
 			printf("定价不合理\n");
 		}
 		else if(passN[pass]==0){       //装入货物
+			k = 0;//
 			passN[pass] = number; 
 			passP[pass] = unitprice;
 			passG[pass] = goods;
 			printf("通道%d装填成功\n",pass);
 			j++;
+			storeH[j] = pass;
 		}
 		else if (passN[pass] != 0) {    //重复安装报错
 			printf("当前通道已有货物请重新放置\n");
 		}
-			storeH[j] = pass;
-			k++;
+		scanf_s("%*[^\n]");
 		scanf_s("%*c");//清缓存区（网上学的）
 	}
 	while(pass > 5 || pass < 1 || unitprice > 9 || unitprice < 1 || goods > 90 || goods < 65||j<5);
-	printf("摆放完毕，开始购买");
+	printf("摆放完毕，开始购买,输入0结束购买，输入99回退一步\n");
 	while (j == 5) {     //不知道怎样写循环条件，只能凑一下了
 		int unitsum = 0;   //单此操作所需金额
 		int end = 0;  //判断是否结束
@@ -68,133 +70,143 @@ int main() {
 		int storeS[4] = { 0 };
 		int storeP[4] = { 0 };
 		int i = 0;//退3步循环
-		j = 0;//总的操作数
+		int a = 0;//总的操作数
 		int jishu(int pass, int passN, int passG, int passP);    //算单次购买货物的函数
 		do {
 			printf("请输入购买通道:");//第二步：购买
 			scanf_s("%d", &pass);
 			switch (pass) {
 			case 1:
-				k = 0;
-				unitprice = 0;
-				unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
-				passN[pass] -= unitsum / passP[pass];   //商品件数
-				sum += unitsum;
-				printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
-				if (i < 3) {                                              //回退，重复了5次代码，应该可以用函数简化，但我不会输出多个值
-					j++;
-					i++;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
+				if (passN[pass] != 0) {
+					k = 0;
+					unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
+					passN[pass] -= unitsum / passP[pass];   //商品件数
+					sum += unitsum;
+					printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
+					if (i < 3) {                                              //回退，重复了5次代码，应该可以用函数简化，但我不会输出多个值
+						a++;
+						i++;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
+					else {
+						a++;
+						i = 1;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
 				}
-				else {
-					j++;
-					i = 1;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
-				}
+				else { printf("当前通道缺货，请换个通道购买\n"); }
 				break;
 			case 2:
-				k = 0;
-				unitprice = 0;
-				unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
-				passN[pass] -= unitsum / passP[pass];
-				sum += unitsum;
-				printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
-				if (i < 3) {
-					j++;
-					i++;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
+				if (passN[pass] != 0) {
+					k = 0;
+					unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
+					passN[pass] -= unitsum / passP[pass];   //商品件数
+					sum += unitsum;
+					printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
+					if (i < 3) {                                              
+						a++;
+						i++;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
+					else {
+						a++;
+						i = 1;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
 				}
-				else {
-					j++;
-					i = 1;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
-				}
+				else { printf("当前通道缺货，请换个通道购买\n"); }
 				break;
 			case 3:
-				k = 0;
-				unitprice = 0;
-				unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
-				passN[pass] -= unitsum / passP[pass];
-				sum += unitsum;
-				printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
-				if (i < 3) {
-					j++;
-					i++;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
+				if (passN[pass] != 0) {
+					k = 0;
+					unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
+					passN[pass] -= unitsum / passP[pass];   //商品件数
+					sum += unitsum;
+					printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
+					if (i < 3) {                                              
+						a++;
+						i++;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
+					else {
+						a++;
+						i = 1;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
 				}
-				else {
-					j++;
-					i = 1;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
-				}
+				else { printf("当前通道缺货，请换个通道购买\n"); }
 				break;
 			case 4:
-				k = 0;
-				unitprice = 0;
-				unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
-				passN[pass] -= unitsum / passP[pass];
-				sum += unitsum;
-				printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
-				if (i < 3) {
-					j++;
-					i++;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
+				if (passN[pass] != 0) {
+					k = 0;
+					unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
+					passN[pass] -= unitsum / passP[pass];   //商品件数
+					sum += unitsum;
+					printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
+					if (i < 3) {                                              
+						a++;
+						i++;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
+					else {
+						a++;
+						i = 1;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
 				}
-				else {
-					j++;
-					i = 1;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
-				}
+				else { printf("当前通道缺货，请换个通道购买\n"); }
 				break;
 			case 5:
-				k = 0;
-				unitprice = 0;
-				unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
-				passN[pass] -= unitsum / passP[pass];
-				sum += unitsum;
-				printf("当前共需要%d元，输入0结束购买,输入99回退一步（最多退3步）\n", sum);
-				if (i < 3) {
-					j++;
-					i++;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
+				if (passN[pass] != 0) {
+					k = 0;
+					unitsum = jishu(pass, passN[pass], passG[pass], passP[pass]);
+					passN[pass] -= unitsum / passP[pass];   //商品件数
+					sum += unitsum;
+					printf("当前共需要%d元，输入0结束购买，输入99回退一步（最多退3步）\n", sum);
+					if (i < 3) {                                              
+						a++;
+						i++;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
+					else {
+						a++;
+						i = 1;
+						storeN[i] = unitsum / passP[pass];
+						storeS[i] = unitsum;
+						storeP[i] = pass;
+					}
 				}
-				else {
-					j++;
-					i = 1;
-					storeN[i] = unitsum / passP[pass];
-					storeS[i] = unitsum;
-					storeP[i] = pass;
-				}
+				else { printf("当前通道缺货，请换个通道购买\n"); }
 				break;
 			case 0:              //结束
 				end = 1;
 				break;
 			case 99:  //回退
-				if (j > 0 && k < 4) {
+				if (a>0&&k < 3) {
 				here:
 					k++;
 					pass = storeP[i];
 					sum -= storeS[i];
 					passN[pass] += storeN[i];
-					j--;
+					a--;
 					if (i > 1) {
 						i--;
 					}
@@ -202,16 +214,23 @@ int main() {
 						i = 3;
 					}
 					printf("已回退一步操作，当前需要%d元\n", sum);
+					break;
 				}
-				else { printf("无法退回\n"); }
-				break;
+				else if(a==0){
+					printf("已退出购买商品阶段，并将最后一次装入的货物退出（按道理顾客不能有这步，但为了写题目）\n");
+					goto there;//我知道goto不能乱用，但我实在想不到别的办法实现跨阶段撤回了
+				}
+				else if (k ==3) {
+					printf("回退次数已用完，无法回退\n");
+					break;
+				}
 			default:
 				printf("不存在此通道，请重新输入/n");
 				break;
 			};
 		} while (end != 1);
 		end = 0;
-		printf("共需要%d元，请投币，如需退回操作，请输入99\n", sum);
+		if (sum != 0) { printf("共需要%d元，请投币，如需退回操作，请输入99\n", sum); }
 		int coin;
 		int sc = 0;   //已投入金额
 		k = 0;//回退元素
@@ -267,7 +286,7 @@ int main() {
 				}
 				break;
 			case 99:
-				if (p > 0 && k < 4) {
+				if (p > 0 && k < 3) {
 					k++;
 					p--;
 					sc -= storeC[q];
